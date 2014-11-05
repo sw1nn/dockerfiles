2eed60f14bb55e5d9315a2c89d76e93109497971
Neale Swinnerton <neale@isismanor.com>
2014-11-05 15:36:53 +0000

    zookeeper updates.

--- zookeeper/start-zookeeper.sh
+++ zookeeper/start-zookeeper.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-LOGDIR="/logs/zookeeper/${HOSTNAME}"
 
-mkdir -p "${LOGDIR}"
+LOG_DIR="/logs/zookeeper/${HOSTNAME}"
+mkdir -p "${LOG_DIR}"
+
+DATA_DIR="/data/zookeeper/${HOSTNAME}"
+mkdir -p ${DATA_DIR}
 
 sed -i \
     -e "s@zookeeper\.log\.dir=.*@zookeeper.log.dir=${LOGDIR}@" \
     -e "s@zookeeper\.tracelog\.dir=.*@zookeeper.log.dir=${LOGDIR}@" \
     /zookeeper/conf/log4j.properties
+
+sed -e "s@dataDir=.*@dataDir=/data/zookeeper/${HOSTNAME}@" \
+    /zookeeper/conf/zoo_sample.cfg > /zookeeper/conf/zoo.cfg
+
 
 /zookeeper/bin/zkServer.sh start-foreground
