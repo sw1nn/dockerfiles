5b892c238360fd8a11718e83c143a1b3598d6169
Neale Swinnerton <neale@isismanor.com>
2014-11-05 15:36:39 +0000

    kafka updates.

--- kafka/start-kafka.sh
+++ kafka/start-kafka.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
+LOG_CONFIG_FILE=/kafka/config/log4j.properties
+
 BROKER_ID=${KAFKA_BROKER_ID:-0}
 
-LOG_DIR="/logs/kafka/${HOSTNAME}/${BROKER_ID}"
-mkdir -p "${LOG_DIR}"
+LOGS_DIR="/logs/kafka/${HOSTNAME}/${BROKER_ID}"
+mkdir -p "${LOGS_DIR}"
 
 DATA_DIR="/data/kafka/${HOSTNAME}/${BROKER_ID}"
 mkdir -p ${DATA_DIR}
 
 sed -i \
-    -e "s@zookeeper.connect=localhost@zookeeper.connect=${ZK01_PORT_2181_TCP_ADDR}@" \
-    -e "s@host.name=localhost@host.name=${HOSTNAME}@" \
-    -e "s@broker.id=0@broker.id=${BROKER_ID}@" \
-    -e "s@log.dirs=/tmp/kafka-logs@log.dirs=${DATA_DIR}@" \
+    -e "s@zookeeper\.connect=localhost@zookeeper.connect=${ZK01_PORT_2181_TCP_ADDR}@" \
+    -e "s@#host\.name=localhost@host.name=${HOSTNAME}@" \
+    -e "s@broker\.id=0@broker.id=${BROKER_ID}@" \
+    -e "s@log\.dirs=/tmp/kafka-logs@log.dirs=${DATA_DIR}@" \
     /kafka/config/server.properties
 
 sed -i \
-    -e "s@kafka.logs.dir=.*@kafka.logs.dir=${LOG_DIR}/@"
-    /kafka/config/log4j.properties
+    -e "s@kafka.logs.dir=.*@kafka.logs.dir=${LOGS_DIR}/@" \
+    -e "/log4j.logger.kafka=INFO, kafkaAppender/d" \
+    ${LOG_CONFIG_FILE}
+
+cat <<EOF >> ${LOG_CONFIG_FILE}
+log4j.appender.logstashAppender=org.apache.log4j.net.SocketAppender
+log4j.appender.logstashAppender.Port=${LOGSTASH_PORT_4561_TCP_PORT}
+log4j.appender.logstashAppender.RemoteHost=${LOGSTASH_PORT_4561_TCP_ADDR}
+log4j.appender.logstashAppender.ReconnectionDelay=30000
+log4j.logger.kafka=INFO, kafkaAppender, logstashAppender
+EOF
 
 /kafka/bin/kafka-server-start.sh /kafka/config/server.properties
